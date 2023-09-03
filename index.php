f449bcfadf2a18a9aed4d5976d2aa037af6fbf05
Omar Vásquez <omar.vasquez@planleft.com>
2023-09-03 18:34:27 -0500

    My first commit

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html class="wide wow-animation desktop landscape rd-navbar-static-linked" lang="en">
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">		
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" type="text/css"
+			href="https://fonts.googleapis.com/css?family=Asap:400,400i,500,500i,600i,700,700i%7CNunito:300,500,900">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/fonts.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body class="" data-new-gr-c-s-check-loaded="14.1122.0" data-gr-ext-installed="">
+		<div class="page animated" style="animation-duration: 500ms;">
+			<header class="section page-header">
+				<!--RD Navbar-->
+				<div class="rd-navbar-wrap" style="height: 102.8px;">
+					<nav class="rd-navbar rd-navbar-classic rd-navbar-original rd-navbar-static rd-navbar--is-stuck"
+						data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed"
+						data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static"
+						data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static"
+						data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px"
+						data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="46px" data-lg-stick-up="true"
+						data-xl-stick-up="true" data-xxl-stick-up="true">
+						
+						<div class="rd-navbar-main-outer">
+							<div class="rd-navbar-main">
+								<!--RD Navbar Panel-->
+								<div class="rd-navbar-panel">
+									<!--RD Navbar Brand-->
+									<div class="rd-navbar-brand">
+										<!--Brand-->										
+										<p style="color: white;font-weight: bold;cursor: pointer;">ATRASO MENSTRUAL PERÚ</p>
+									</div>
+								</div>
+								<div class="rd-navbar-main-element">
+									<div class="rd-navbar-nav-wrap toggle-original-elements">
+										<ul class="rd-navbar-nav">
+											<li class="rd-nav-item"><a class="rd-nav-link" href="https://api.whatsapp.com/send?phone=51902259174&text=Hola,%20quiero%20sus%20servicios" target="_blank" style="font-weight: bold;">LLAMANOS AL 902 259 174</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</header>
+			<!--Intro-->
+			<section class="section-lg bg-image-1 section-intro">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8">
+							<div class="intro-box wow fadeInLeft" data-wow-dalay=".2s"
+								style="visibility: visible; animation-name: fadeInLeft;"><span class="subtitle">Bienvenida a</span>
+								<h1>ATRASO MENSTRUAL PERÚ</h1>
+								<h2>Nuestros especialistas te estan esperando</h2>
+								<div class="button-group">
+									<a class="button-primary button button-md" href="https://api.whatsapp.com/send?phone=51902259174&text=Hola,%20quiero%20sus%20servicios" target="_blank">HAZ CLIC AQUÍ PARA AGENDER CITA!</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--Service-creative-->
+			<section class="section section-sm section-negative bg-default">
+				<div class="container">
+					<div class="row row-narrow-12 row-30 justify-content-center">
+						<div class="col-12 col-lg-6 col-xl-4 wow fadeInUp" data-wow-dalay=".1"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="icon-box-creative secondary">
+								<div class="icon-box-creative__info">
+									<div class="icon-box-creative__media"><img src="images/service-1-34x44.png" alt=""
+											width="34" height="44">
+									</div>
+									<div class="icon-box-creative__title">Personal Calificado</div>
+									<p style="text-transform: uppercase;">Nuestro especialistas se encuentran listos para atenderte.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-lg-6 col-xl-4 wow fadeInUp" data-wow-dalay=".2"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="icon-box-creative secondary-light">
+								<div class="icon-box-creative__info">
+									<div class="icon-box-creative__media"><img src="images/service-2-48x34.png" alt=""
+											width="48" height="34">
+									</div>
+									<div class="icon-box-creative__title">Emergencias</div>
+									<p style="text-transform: uppercase;">Estamos listos para atender todo tipo de urgencias</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-lg-6 col-xl-4 wow fadeInUp" data-wow-dalay=".3"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="icon-box-creative primary">
+								<div class="icon-box-creative__info">
+									<div class="icon-box-creative__media"><img src="images/service-3-35x44.png" alt=""
+											width="35" height="44">
+									</div>
+									<div class="icon-box-creative__title">Las 24 Horas</div>
+									<p>ATENDEMOS LAS 24 HORAS: <a style="color: white;" href="https://api.whatsapp.com/send?phone=51902259174&text=Hola,%20quiero%20sus%20servicios">902259174</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--About-->
+			<section class="section bg-default">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-xl-6 col-lg-10 wow fadeInUp" data-wow-dalay=".1s"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="box-about-creative section-lg">								
+								<h2 class="wow fadeInLeft" data-wow-dalay=".2s"
+									style="visibility: visible; animation-name: fadeInLeft;">Solución <span
+										class="text-primary">Inmediata</span></h2>
+								<p class="text-md">Si se tiene un retraso menstrual inesperado la solución es posible te ayudamos a resolverlo con procedimientos seguros y confiables ya que contamos con métodos modernos y seguros que brindan total seguridad ya que contamos con médicos especializados que aseguran un tratamiento adecuado y eficaz.</p>
+								<div class="row row-xl-30">
+									<div class="col-lg-6">
+										<ul class="list-marked">
+											<li>Atención Medica</li>
+											<li>Solución Inmediata</li>
+											<li>Analisis de Ciclo</li>
+											<li>Doctores</li>											
+										</ul>
+									</div>
+									<div class="col-lg-6">
+										<ul class="list-marked">
+											<li>Diagnosticos</li>
+											<li>Precios Accesibles</li>
+										</ul>
+									</div>
+									<div class="col-12"><a class="button button-primary btn-sm button-sm"
+											href="https://api.whatsapp.com/send?phone=51902259174&text=Hola,%20quiero%20sus%20servicios">HAZ CLIC AQUÍ PARA AGENDER CITA!</a></div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-6 d-none d-xl-block wow fadeInUp" data-wow-dalay=".2s"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="box-about-creative__media"><img src="images/home-img-1-682x625.png" alt=""
+									width="682" height="625">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!--CTA-->
+			<section class="section bg-gray-light">
+				<div class="container">
+					<div class="row row-narrow-80 flex-column-reverse flex-lg-row align-items-center">
+						<div class="col-lg-5 d-none d-lg-flex align-items-end">
+							<div class="media-creative wow fadeInLeft"
+								style="visibility: visible; animation-name: fadeInLeft;"><img
+									src="images/atraso9.png" alt="" width="499" height="524">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="section-lg text-center text-lg-left">
+								<h2 class="wow fadeInRight" style="visibility: visible; animation-name: fadeInRight;">
+									TÚ TIENES LA <span class="text-primary">DECISION</span> NO LO PIENSES MÁS.</h2>
+								<p class="text-dark text-md wow fadeInRight" data-wow-dalay=".2s"
+									style="visibility: visible; animation-name: fadeInRight;">Doctoras ginecologas especialistas en regularizar la menstruacion al instante y sin dolor.</p><a
+									class="wow fadeInRight button-primary button button-md" href="https://api.whatsapp.com/send?phone=51902259174&text=Hola,%20quiero%20sus%20servicios"
+									data-wow-dalay=".2s" style="visibility: visible; animation-name: fadeInRight;">HAZ CLIC AQUÍ PARA AGENDER CITA!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!--News-->
+			<section class="section section-lg">
+				<div class="container">
+					<h2 class="text-center text-md-left wow fadeInLeft" data-wow-dalay=".2s"
+						style="visibility: visible; animation-name: fadeInLeft;"><span class="text-primary">NO ESPERES MÁS</span> REGULARIZAMOS TU ATRASO MENSTRUAL Y SIGUE CON TU VIDA NORMAL</h2>
+					<div class="row row-30 justify-content-center">
+						<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp" data-wow-dalay=".2s"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="news-box">
+								<div class="news-box__media"><img
+											src="images/atraso-menstrual.jpg" alt="" width="570" height="450">
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp" data-wow-dalay=".4s"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="news-box">
+								<div class="news-box__media"><img
+											src="images/atraso-menstrual2.jpg" alt="" width="570" height="450">
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp" data-wow-dalay=".6s"
+							style="visibility: visible; animation-name: fadeInUp;">
+							<div class="news-box">
+								<div class="news-box__media"><img
+											src="images/atraso-menstrual-3.jpg" alt="" width="570" height="450">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!--Footer-->
+			<footer class="section footer-classic context-dark">
+				<div class="footer-classic__body">
+					<div class="container">
+						<div class="row row-30">
+							<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp">
+								<h6 class="title-decorated wow fadeInUp"
+									style="visibility: visible; animation-name: fadeInUp;">LOCALES</h6>
+								<ul class="footer-nav-list">
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Lima</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Comas</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en SJL</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en los Olivos</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Surco</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Salamanca</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en SMP</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Ceneguilla</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Barranco</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Miraflores</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Chorillos</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Villa el Salvador</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Villa Maria</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Jose Galvez</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Huachipa</li>
+								</ul>
+							</div>
+							<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp">
+								<h6 class="title-decorated wow fadeInUp"
+									style="visibility: visible; animation-name: fadeInUp;">LOCALES</h6>
+								<ul class="footer-nav-list">
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso  en Cercado de Lima</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Abancay</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Tacna</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso en Independencia</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Callao</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en la Parada</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Gamarra</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual los conos</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Rimac</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Grau</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en San Borja</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Jose Galvez</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Tablada</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Lurin</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual ATE</li>
+								</ul>
+							</div>
+							<div class="col-sm-8 col-lg-4 col-md-6 wow fadeInUp">
+								<h6 class="title-decorated wow fadeInUp"
+									style="visibility: visible; animation-name: fadeInUp;">LOCALES</h6>
+								<ul class="footer-nav-list">
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso  en Jirón de la Unión</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Chorillos</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en pascana</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en provincia</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso en Jesus Maria</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en San Miguel</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso  en Puente Piedra</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual La Victoria</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en Barranco</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso  en Caja de Agua</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual en San Luis</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Santa Clara</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Ñaña</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Chaclacayo</li>
+									<li class="wow fadeInUp" data-wow-dalay=".6s" style="visibility: visible; animation-name: fadeInUp;">Atraso menstrual Cerro camote</li>
+								</ul>
+							</div>							
+						</div>
+					</div>
+				</div>
+				<div class="footer-classic__aside">
+					<div class="container">
+						<div class="row row-10 flex-column-reverse flex-sm-row">
+							<div class="col-sm-6 text-center text-md-left wow fadeInLeft"
+								style="visibility: visible; animation-name: fadeInLeft;">
+								<p class="rights"><span>©&nbsp; </span><span
+										class="copyright-year">2023</span><span>&nbsp;</span><span>ATRASO MENSTRUAL PERÚ</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>		
+		<script src="js/core.min.js"></script>
+		<script src="js/script.js"></script>	
+		<a href="#" id="ui-to-top" class="ui-to-top fa fa-angle-up active"></a>	
+	</body>
+</html>
